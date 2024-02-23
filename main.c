ae309749c8410d8aadd4b333e7b74d26e3647be4
Agustin Paternoster <apaterno@cbr10s5.42barcelona.com>
2024-02-23 14:02:43 +0100

    test

--- main.c
+++ main.c
@@ -3,23 +3,48 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-int main(void)
+int find_newLine(char *str, int size)
 {
-	int bf = 10;
+	int i;
+
+	while(i < size )
+	{
+		
+	}	
+}
+
+
+void get_next_line(fd)
+{
+	int bf = 25;
 	char buffer[bf];
 	int i = 0;
 	int fd = open("./test.txt",O_RDONLY);
 	int ch = -1;
+	int Cline = 0
+	static int readline = 0
 
-	while ( ch != 0 )
+	while ( ch != 0)
 	{	
 		ch = read(fd,buffer,bf);
+		if (cline 
 		int i = 0;
-		while (buffer[i] != '\n')
+		while (buffer[i] != '\n' & i < bf & ch !=0  )
 		{	
 			write(1,&buffer[i],1);
 			i++;
 		}
+		if (buffer[i] == '\n')
+			cline++
 	}
+	line++
+}
+
+
+int main(void)
+{
+	
+	int fd = open("./test.txt",O_RDONLY);
+
 	return (0);
 }
